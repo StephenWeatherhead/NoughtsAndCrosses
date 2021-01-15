48fa4cb034eab353affbc11fe1e6ff451583b538
Stephen Weatherhead <stephenw42@outlook.com>
2021-01-15 23:18:40 +0000

    Game can now be played by 2 players

--- NoughtsAndCrosses/Program.cs
+++ NoughtsAndCrosses/Program.cs
@@ -6,7 +6,17 @@ namespace NoughtsAndCrosses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Noughts and Crosses!");
+            Game game = new Game();
+            while (game.GameState == GameState.NoWin)
+            {
+                Console.WriteLine(game);
+                string move = Console.ReadLine().ToLower();
+                game.Mark(game.CurrentPlayer, "abc".IndexOf(move[0]), "012".IndexOf(move[1]));
+            }
+            Console.WriteLine(game);
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
         }
     }
 }
